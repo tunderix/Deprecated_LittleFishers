82926fe297f03fd1390ff88dd6ab2a537fcd5167
Sampsoni <sami.repo@live.com>
2020-12-09 19:23:47 +0200

    Setup for VSCode and Lots of graphics

---
+++ LittleFishers/Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    int something = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Happened½");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 some = new Vector3();
+        
+    }
+
+    private void Awake() {
+        Debug.Log("This happened first " + something);
+    }
+}
