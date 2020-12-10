9caadcf642da9e7a4ab00e9279c3de66f0155c8a
Sampsoni <sami.repo@live.com>
2020-12-10 19:02:53 +0200

    Trying out inventory objects as Scriptable Objects

---
+++ LittleFishers/Assets/ScriptableObjects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType itemType;
+    [TextArea(15, 20)]
+    public string description;
+}
---
+++ LittleFishers/Assets/ScriptableObjects/Items/Scripts/ItemType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum ItemType
+{
+    Food,
+    Equipment,
+    Default
+}
