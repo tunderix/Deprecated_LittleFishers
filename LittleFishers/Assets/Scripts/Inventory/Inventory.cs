878d5d9c4de64a3034ceafd4ceaf3acc4f41d7d5
Sampsoni <sami.repo@live.com>
2021-01-12 17:59:53 +0200

    Temporary

--- LittleFishers/Assets/Scripts/Inventory/Inventory.cs
+++ LittleFishers/Assets/Scripts/Inventory/Inventory.cs
@@ -1,11 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-namespace LittleFishers
+namespace LittleFishers.LFInventory
 {
-    public class Inventory
+    [System.Serializable]
+    public class Inventory : IInventory
     {
+        private string _inventoryName;
+        private int _inventorySize;
+        private int _gold;
+        private Dictionary<InventorySlot, InventoryItemStack> _inventoryItems;
 
-    }
-}
+        public string InventoryName { get => _inventoryName; set => _inventoryName = value; }
+        public int InventorySize { get => _inventorySize; set => _inventorySize = value; }
+        public int Gold { get => _gold; set => _gold = value; }
+
+
+        public Inventory(InventoryTemplate template)
+        {
+            _inventoryName = template.InventoryName;
+            _inventorySize = template.InventoryCapacity;
+            _gold = template.Gold;
+        }
+        
+        {
+        After:
+            public bool AddItem(InventoryItem 
+            {
+                */
+            }
+                {
+
+                }
+
+                }
+                {
+            }
+        }
